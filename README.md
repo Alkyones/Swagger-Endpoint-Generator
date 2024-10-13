2ab584088001a68eba99fa31b4eeec5536b87aad
Atakan Yildirim <atakan.yildirim@frupro.com>
2024-10-13 20:05:26 +0300

    docs: Updated README with schema generator Added schema generator section Improved documentation clarity

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Swagger-Endpoint-Generator
-A console script that allows you to create an openApiDefinition endpoint simple and fast.
+A script package contains a console script that allows you to create an openApiDefinition endpoint and endpoint's definitions simple and fast.
 
-# Code Run 
+# Endpoint Generator
+## Code Run 
 -This is a console script with required arguments, details of arguments can be seen in the code or console. below you can see an example for running the script.
 ![preview](/assets/code-example.png)
 
 # Response Example
 -After succesfull response generated yaml copied in the clipboard.
 ![preview](/assets/result-example.png)
+
+# Schema Definition Generator
+## Code Run 
+-This is a console script with required path for schema definition json file.
+```py schemaGenerator.py --json-file=schemaInput.json```
+
+# Response Example
+-After succesfull response generated yaml copied in the clipboard.
+![preview](/assets/result-example-schema.png)
---
+++ schemaGenerator.py
@@ -0,0 +1,66 @@
+import argparse
+import json
+import yaml
+import pyclip
+
+TYPE_MAPPING = {
+    'str': 'string',
+    'int': 'integer',
+    'float': 'number',
+    'list': 'array',
+    'dict': 'object',
+    'bool': 'boolean',
+    'NoneType': 'null',
+}
+
+def create_swagger_definition(schema_name, schema_type, schema_data):
+    swagger_definition = {
+        "type": TYPE_MAPPING.get(schema_type, schema_type),
+    }
+
+    if schema_type == "object":
+        properties = {}
+        for key, value in schema_data.items():
+            print(value)
+            if isinstance(value, dict):
+                properties[key] = create_swagger_definition(f"{schema_name}{key}", "object", value)
+            elif isinstance(value, list):
+                properties[key] = {
+                    "type": "array",
+                    "items": create_swagger_definition(f"{schema_name}{key}", "object", value[0])
+                }
+            else:
+                properties[key] = {"type": f"{TYPE_MAPPING.get(type(value).__name__, type(value).__name__.lower())}"}
+        swagger_definition["properties"] = properties
+    elif schema_type == "array":
+        swagger_definition["items"] = create_swagger_definition(schema_name, "object", schema_data[0])
+    return swagger_definition
+
+def create_swagger_definitions(json_file):
+    with open(json_file, 'r') as f:
+        schema_data = json.load(f)
+
+    swagger_definitions = {}
+    for schema_name, schema in schema_data.items():
+        if schema_name == 'request':
+            print(schema)
+            request_definition = create_swagger_definition(f"{schema.get("name")}{schema_name.title()}", "object", schema["data"])
+            swagger_definitions[f"{schema["name"]}Request"] = request_definition
+        if schema_name == 'response':
+            response_definition = create_swagger_definition(f"{schema.get("name")}{schema_name.title()}", "object", schema["data"])
+            swagger_definitions[f"{schema["name"]}Response"] = response_definition
+
+    return swagger_definitions
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json-file", required=True, help="JSON file containing schema data")
+
+    args = parser.parse_args()
+
+    swagger_definitions = create_swagger_definitions(args.json_file)
+
+    pyclip.copy(
+        yaml.dump({"definitions": swagger_definitions}, default_flow_style=False, sort_keys=False)
+    )
+    print("Generated Swagger definitions copied to clipboard...")
